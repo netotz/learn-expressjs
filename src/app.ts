16a0e99f873bab96b81b216bea2818d7c0ff8802
netotz <neto.otz@hotmail.com>
2022-09-12 23:24:12 -0500

    Add POST endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => response.send("Hello from Express!"));
+
+app.post("/api/data", (request, response) => {
+    console.log(request.body);
+    return response.sendStatus(200);
+});
 
 app.listen(3000, () => {
     console.log("Application listening...");
